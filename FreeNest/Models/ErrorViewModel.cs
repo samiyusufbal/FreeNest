3f440dcecd72311603cf7c9c4a46cd54fe8ab80e
samiyusufbal <samiyusufbal@gmail.com>
2025-08-19 21:58:11 +0300

    Add project files.

---
+++ FreeNest/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FreeNest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
